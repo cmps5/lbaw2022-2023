70d104b2801a6998739cb07d07eebad114666008
Matias FG <matiasfreitasguimaraes@gmail.com>
2022-11-22 15:32:49 +0000

    Some Controllers updates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,5 +4,29 @@ namespace App\Http\Controllers;
 
 class UserController
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except('show');
+    }
 
+
+    public function show($id)
+    {
+        $user = User::find($id);
+        return view('user.show', compact('user'));
+    }
+
+
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('user.edit', compact('user'));
+    }
+
+    public function destroy($id)
+    {
+        User::where('id', $id)->delete();
+
+        return Redirect::to(RouteServiceProvider::HOME, 200);
+    }
 }
