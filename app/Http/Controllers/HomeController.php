536bac21cd54dd4c28ee933a294e9d07b8592e6c
Matias FG <matiasfreitasguimaraes@gmail.com>
2022-11-22 15:22:40 +0000

    Some Controllers updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,26 @@
 
 namespace App\Http\Controllers;
 
-class HomeController
+class HomeController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
 
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function sortedPosts()
+    {
+        $posts = Post::all()->sortByDesc('votes');
+
+        return view('home', compact('posts'));
+    }
 }
--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,4 +5,59 @@ namespace App\Http\Controllers;
 class SearchController
 {
 
+    public function index()
+    {
+        $posts = Post::all()->sortByDesc('votes');
+        $users = User::all()->sortByDesc('reputation');
+        return view('searches.index', compact('posts', 'users'));
+    }
+
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'content' => ['required', 'string'],
+        ]);
+
+        auth()->user()->searches()->create($data);
+
+        return Redirect('searches/' . $data['content'],302);
+    }
+
+
+    public function show($content)
+    {
+        $posts = Post::search($content)->get();
+
+        //dd($posts);
+        return view('searches/index', compact('posts'));
+    }
+
+
+    public function update(Request $request, Search $search)
+    {
+        //ToDo
+    }
+
+
+    public function destroy(Search $search)
+    {
+        //ToDo
+    }
+
+
+    public function search(Request $request)
+    {
+        $content = $request['content'];
+        $posts = DB::table('post')
+            ->select(
+                'post.',
+                'post.tsvectors as post_tsvectors',)
+            ->selectRaw("*, ts_rank(post.tsvectors, plainto_tsquery('english', '$content')) as rank")
+            ->whereRaw("post.tsvectors @@ to_tsquery('english', '$content')")
+            ->orderBy('rank', 'DESC')
+            ->get();
+
+        return view('search.' . $content, compact('posts'));
+    }
 }
