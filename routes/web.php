26d11ac1354ce1dd20bd54dff0b3d36df862c3ac
Matias FG <matiasfreitasguimaraes@gmail.com>
2022-11-22 15:10:10 +0000

    routes/web.php created basic methods and controllers files created.

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,56 @@
 |
 */
 
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Auth::routes();
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Static pages
+Route::get('/about', function () {
+    return view('static.about');
+})->name('about');
+Route::get('/contacts', function () {
+    return view('static.contacts');
+})->name('contacts');
+Route::get('/help', function () {
+    return view('static.help');
+})->name('help');
+Route::get('/features', function () {
+    return view('static.features');
+})->name('features');
+
+// User
+Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::patch('/users/{user}', [UserController::class, 'update'])->name('users.update');
+Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+Route::delete('/users/{user}/delete', [UserController::class, 'delete'])->name('users.delete');
+//needs improvement
+Route::patch('/users/{user}', [UserController::class, 'extendTimeout'])->name('users.extendTimeout');
+
+// Post
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::patch('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+// Search
+Route::get('/searches', [SearchController::class, 'index'])->name('search.index');
+Route::get('/searches/{search}', [SearchController::class, 'show'])->name('search.show');
+Route::post('/searches', [SearchController::class, 'store'])->name('search.store');
+
+// --------------------------------------------
+// Report
+Route::get('/reports', function () {
+    return view('report');
+});
