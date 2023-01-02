6d9f866421aa19692b2a07bc38d2cf980c03c71f
matiasfg <matiasfreitasguimaraes@gmail.com>
2023-01-02 20:45:48 +0000

    some bugs corrected

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,21 +4,41 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\Search;
+use App\Models\Tag;
 use App\Models\User;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Types\True_;
 
 class SearchController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth')->only('index', 'store', 'show', 'search');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $posts = Post::all()->sortByDesc('votes');
         $users = User::all()->sortByDesc('reputation');
-        return view('searches.index', compact('posts', 'users'));
+        $content = 'all';
+        return view('searches.index', compact('posts', 'users', 'content'));
     }
 
-
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         $data = $request->validate([
@@ -30,40 +50,60 @@ class SearchController extends Controller
         return Redirect('searches/' . $data['content'],302);
     }
 
-
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Search  $search
+     * @return \Illuminate\Http\Response
+     */
     public function show($content)
     {
         $posts = Post::search($content)->get();
-
+        $tags = Tag::all();
         //dd($posts);
-        return view('searches/index', compact('posts'));
+        return view('searches/index', compact('posts', 'tags', 'content'));
     }
 
-
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Search  $search
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, Search $search)
     {
-        //ToDo
+        //
     }
 
-
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Search  $search
+     * @return \Illuminate\Http\Response
+     */
     public function destroy(Search $search)
     {
-        //ToDo
+        //
     }
 
 
-    public function search(Request $request)
-    {
-        $content = $request['content'];
-        $posts = DB::table('post')
-            ->select(
-                'post.',
-                'post.tsvectors as post_tsvectors',)
-            ->selectRaw("*, ts_rank(post.tsvectors, plainto_tsquery('english', '$content')) as rank")
-            ->whereRaw("post.tsvectors @@ to_tsquery('english', '$content')")
-            ->orderBy('rank', 'DESC')
-            ->get();
 
-        return view('search.' . $content, compact('posts'));
+    public function filter(Request $request, $content)
+    {
+        $posts = Post::search($content)->get();
+
+        $posts = $posts->filter(function ($post) use ($request) {
+            foreach ($post->tags as $tag){
+                $tagname = 'tag' . $tag->id;
+                if($request[$tagname])
+                    return true;
+            }
+            return false;
+        });
+
+        $tags = Tag::all();
+
+        return view('searches/index', compact('posts', 'tags', 'content'));
     }
 }
